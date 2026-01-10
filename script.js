178f326bad834028a624038a6247dce55d32e9d2
Daniel Enikuomehin <danieloluwatobafunmi@gmail.com>
2026-01-10 10:20:18 +0100

    Updated portfolio content for professional CV

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const mySkills = ['HTML','CSS','Flexbox','Grid','Javascript'];
+const mySkills = [
+    'HTML & CSS', 
+    'JavaScript', 
+    'Embedded Systems', 
+    'C++ / C', 
+    'Circuit Design', 
+    'Git & GitHub'
+];
 const list = document.getElementById('skills-list');
 mySkills.forEach(skill => {
     const newLi = document.createElement('li');
